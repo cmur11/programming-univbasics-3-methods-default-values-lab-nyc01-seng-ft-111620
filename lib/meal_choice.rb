2e381ea1195ccc18fbcd3ed97ec5acc3e0b43661
Conor Murnane <conormurnane11@gmail.com>
2020-10-27 22:04:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,5 +6,5 @@ def meal_choice(veg1,veg2,protein="tofu")
     puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice(broccoli,macaroni)
-meal_choice(broccoli,macaroni,roast beef)
+meal_choice("broccoli","macaroni")
+meal_choice("broccoli","macaroni","roast beef")
